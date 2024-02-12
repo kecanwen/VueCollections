08fa0cda9c24f80208dae65aa589a9b516073883
kecanwen <kcw199611252815@163.com>
2024-02-12 23:34:29 +0800

    update

---
+++ handWriteJs/src/js/checkIfInstanceOf.js
@@ -0,0 +1,6 @@
+// !编写一个函数，检查给定的值是否是给定类或超类的实例 
+var checkIfInstanceOf = function (obj, classFunction) {
+    if (obj === null || obj === undefined || !(classFunction instanceof Function))
+      return false;
+    return Object(obj) instanceof classFunction;
+  };
